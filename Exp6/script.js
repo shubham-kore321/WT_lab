84befd574d8c18101db9d6533c8c034b3317a020
shubham kore <shubhamkore321@gmail.com>
2026-03-03 11:18:11 +0530

    changes

---
+++ Exp6/script.js
@@ -0,0 +1,12 @@
+function show() {
+
+    document.getElementById("output").innerHTML +=
+        "<br>Name: " + name.value +
+        "<br>Email: " + email.value +
+        "<br>Password: " + pass.value +
+        "<br>DOB: " + dob.value +
+        "<br>Country: " + country.value +
+        "<br>Address: " + address.value +
+        "<br><hr>";
+
+}
